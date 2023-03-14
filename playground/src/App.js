c7149ee68b0506fe0effae913dc0c3f1a61eb868
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-03-14 16:01:20 +0000

    added state in ListGroup component

--- playground/src/App.js
+++ playground/src/App.js
@@ -2,7 +2,6 @@ import ListGroup from "./components/ListGroup";
 function App() {
   return (
     <>
-    <h1>React Playground</h1>
     <ListGroup />
     </>
 
--- playground/src/components/ListGroup.js
+++ playground/src/components/ListGroup.js
@@ -1,19 +1,43 @@
 import React from "react";
 
+// The React useState Hook allows us to track state in a function component.
+// State generally refers to data or properties that need to be tracking in an application.
+import { useState } from "react";
+
 // List group component
 function ListGroup() {
-  let fruits = [];
+  let fruits = ["apples", "oranges", "pears", "bananas"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [fruit, setFruit] = useState("");
+
   return (
-    <div>
+    <div className="w-50 mx-auto">
       <h3>Fruits List</h3>
+
+      {fruit !== "" ? (
+        <h5>You clicked {fruit}</h5>
+      ) : (
+        <h5>Click a fruit</h5>
+      )}
       {/* If there are 0 fruits display no fruits available */}
       {fruits.length <= 0 && <p>No Fruits available</p>}
 
       {/* List group */}
       <ul className="list-group">
-        {fruits.map((item) => (
-          <li className="list-group-item" key={item}>
-            {item}
+        {fruits.map((fruit, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+                setSelectedIndex(index);
+              setFruit(fruit);
+            }}
+            key={fruit}
+          >
+            {fruit}
           </li>
         ))}
       </ul>
