e99753b5d9f48384be5e9e1daf8e550a5abc1bda
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-03-14 14:33:33 +0000

    added conditioning in list group component

--- playground/src/components/ListGroup.js
+++ playground/src/components/ListGroup.js
@@ -1,15 +1,24 @@
 import React from "react";
 
+// List group component
 function ListGroup() {
-    let items = ['apples', 'oranges', 'pears', 'bananas'];
-    return (
-        <div>
-            <h1>List Group</h1>
-            <ul>
-                {items.map(item => <li key={item}>{item}</li>)}
-            </ul>
-        </div>
-    );
+  let fruits = [];
+  return (
+    <div>
+      <h3>Fruits List</h3>
+      {/* If there are 0 fruits display no fruits available */}
+      {fruits.length <= 0 && <p>No Fruits available</p>}
+
+      {/* List group */}
+      <ul className="list-group">
+        {fruits.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListGroup;
